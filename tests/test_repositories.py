57ce2770870690c50645884acc5c428215c40daf
Laura <lmarinvelez0@gmail.com>
2026-04-20 17:47:32 -0500

    feat: agrego test para repositories

---
+++ tests/test_repositories.py
@@ -0,0 +1,18 @@
+import pytest
+from .repositories import IProductRepository, IChatRepository
+
+
+class TestIProductRepository:
+
+    def test_cannot_instantiate_directly(self):
+        """IProductRepository no puede instanciarse directamente"""
+        with pytest.raises(TypeError):
+            IProductRepository()
+
+
+class TestIChatRepository:
+
+    def test_cannot_instantiate_directly(self):
+        """IChatRepository no puede instanciarse directamente"""
+        with pytest.raises(TypeError):
+            IChatRepository()
